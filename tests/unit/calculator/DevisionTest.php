30656795cdff8b687ba35dca68a492f72b7f3460
adiputra22 <tuts.adiputra@gmail.com>
2018-06-24 14:52:28 +0700

    Testing a calculator: Division

---
+++ tests/unit/calculator/DevisionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DevisionTest extends TestCase
+{
+    public function test_divides_given_operands()
+    {
+        $division = new App\Calculator\Division;
+        $division->setOperands([100,2]);
+
+        $this->assertEquals(50, $division->calculate());
+    }
+
+    public function test_removes_division_by_zero_operands()
+    {
+        $division = new App\Calculator\Division;
+        $division->setOperands([100,0,0,2,0]);
+
+        $this->assertEquals(50, $division->calculate());       
+    }
+
+    public function test_no_operands_given_throws_exception_when_calculating()
+    {
+        $this->expectException(\App\Calculator\Exceptions\NoOperandsException::class);
+        $division = new App\Calculator\Division;
+        $division->calculate();
+    }
+}
